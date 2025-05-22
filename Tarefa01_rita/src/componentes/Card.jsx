7b10231e393f13a7e3680ae61f8786c75905a90e
mariasjs <161545922+mariasjs@users.noreply.github.com>
2025-05-22 17:59:08 -0300

    Tarefa01

---
+++ Tarefa01_rita/src/componentes/Card.jsx
@@ -0,0 +1,15 @@
+export default function Card (props){
+
+    return(
+        <>
+        <div>
+            <figure>
+                <img src={props.foto} alt="BobEsponja"/>
+                <figcaption>Bobesponja</figcaption>
+            </figure>
+        </div>
+
+    </>
+    );
+
+}
