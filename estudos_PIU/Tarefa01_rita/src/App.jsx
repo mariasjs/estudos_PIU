c04959dc9a0a79228f3d52fa6159abee870b1b99
mariasjs <161545922+mariasjs@users.noreply.github.com>
2025-05-25 14:08:37 -0300

    Tarefa01_refeita

---
+++ estudos_PIU/Tarefa01_rita/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Card from './componentes/Card'
+import './App.css'
+import getImageUrl from './assets/bobesponja.png'
+
+function App() {
+
+  return (
+    <>
+      <Card foto ={getImageUrl}/>
+    
+    </>
+
+  )
+}
+
+export default App
