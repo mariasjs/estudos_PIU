411b2f66636eb3b30f9dab1e1b85f1aa91e4d1d9
mariasjs <lucenamaria767@gmail.com>
2025-06-26 17:49:50 -0300

    "manipulando_inputs"

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Form from './componentes/Form'
+import FormState from './componentes/FormState'
+
+function App() {
+
+  return (
+    <>
+      <FormState/>
+    </>
+  )
+}
+
+export default App
---
+++ src/componentes/Form.jsx
@@ -0,0 +1,20 @@
+
+export default function Form(){
+    
+    return(
+        <div>
+            
+    <form>
+
+        <div>
+
+        <label htmlFor="tarefa">Tarefa</label>
+        <input type="text" name='tarefa' placeholder='Digite sua tarefa' />
+
+        </div>
+            <input type="submit" value="Enviar"/>
+    </form>
+        </div>
+    )
+
+}
